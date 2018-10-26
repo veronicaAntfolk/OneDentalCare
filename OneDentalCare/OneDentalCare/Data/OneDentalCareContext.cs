3391c1d64b6f4624f581749628837c7b318d4014
Veronica Antfolk <veronica.antfolk@gmail.com>
2018-10-26 12:55:08 +0200

    Added patient model, controller, views and created database

---
+++ OneDentalCare/OneDentalCare/Data/OneDentalCareContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace OneDentalCare.Models
+{
+    public class OneDentalCareContext : DbContext
+    {
+        public OneDentalCareContext (DbContextOptions<OneDentalCareContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OneDentalCare.Models.Patient> Patient { get; set; }
+    }
+}
---
+++ OneDentalCare/OneDentalCare/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneDentalCare.Models
+{
+    public class Patient : Person
+    {
+        public string Journal { get; set; }
+
+    }
+}
